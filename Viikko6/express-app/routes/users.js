2404bc6ddec10d670fd1a0fd466a2f7d81e31412
ZesseX <jesse.mikkola@live.fi>
2023-02-22 19:37:31 +0200

    Viikko 6 teht 15

---
+++ Viikko6/express-app/routes/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+const { response } = require('../app');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.use(function (request, response, next){
+  console.log("Olen middleware 1");
+  next();
+});
+
+router.get('/toka', function(request,response){
+  response.send("Toka methodi");
+  console.log("Toka methodi");
+});
+
+router.get('/kolmas/:nimi', function(request,response){
+  response.send("Olen" + request.params.nimi);
+  console.log(request.params.nimi);
+});
+
+router.get('/neljas/:enimi/:snimi', function(request, response){
+  response.send("Olen" + request.params.enimi + " " + request.params.snimi);
+});
+
+router.post('/', function(request, response){
+  response.send(request.headers);
+  console.log("post methodi");
+});
+
+module.exports = router;
